6e21aa9e41abbee64d87b689195f3440ff98b7f9
S <s>
2017-11-04 00:37:38 -0700

    {"types":"A","runs":29}

---
+++ GraphicsContest.java
@@ -0,0 +1,15 @@
+/*
+ * File: GraphicsContest.java
+ * --------------------------
+ */
+
+import acm.program.*;
+import acm.graphics.*;
+
+public class GraphicsContest extends GraphicsProgram {
+
+	public void run() {
+		/* Empty */
+	}
+
+}
