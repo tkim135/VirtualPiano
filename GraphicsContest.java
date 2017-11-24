cad211b7d5fe775198fbdc6f16274e51247eae30
S <s>
2017-11-24 12:51:36 -0800

    {"types":"C","runs":26}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -22,7 +22,14 @@ public class GraphicsContest extends Program implements PianoConstants {
 	}
 	
 	public void mouseClicked(MouseEvent e) {
-		bottomHalfSounds(e.getX(), e.getY());
+		createBottomHalfSounds(e.getX(), e.getY());
+	}
+	
+	private void createBottomHalfSounds(int x, int y) {
+		if (x > 0 && x < getWidth() / NUM_WHITE && y > getHeight() / 2 && y < getHeight()) {
+			AudioClip cFour = MediaTools.loadAudioClip("Marimba.yarn.ff.C4.stereo.aif");
+			cFour.play();
+		}
 	}
 	
 	/* add record feature, keyboard changes color, gliss? (connect button to key 
