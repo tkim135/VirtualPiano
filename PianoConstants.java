051b3f39cee2c9dc896c6e4694cc7e3373e4b805
S <s>
2017-11-24 11:48:44 -0800

    {"types":"C","runs":2}

--- PianoConstants.java
+++ PianoConstants.java
@@ -4,7 +4,7 @@ public interface PianoConstants {
 	/** The width of the application window */
 	public static final int APPLICATION_WIDTH = 800;
 	
-	/** */
+	/**  */
 	
 	/** Number of white keys */
 	public static final int NUM_WHITE = 8;
