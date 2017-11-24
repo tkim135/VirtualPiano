1f33119c2f2a3b1af2303a98ea928e5e1bd6a475
S <s>
2017-11-24 10:12:50 -0800

    {"types":"C","runs":0}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -5,12 +5,14 @@
 
 import acm.program.*;
 import acm.util.*;
+import javafx.scene.media.AudioClip;
 import acm.graphics.*;
 
 public class GraphicsContest extends GraphicsProgram {
 
 	public void run() {
 		AudioClip cFour = MediaTools.loadAudioClip("Marimba.yarn.ff.C4.stereo.aif");
+		cFour.play();
 	}
 
 }
