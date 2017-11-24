af4a803ea018b460868477dd54a3fb7ccd8e6406
S <s>
2017-11-24 12:32:49 -0800

    {"types":"C","runs":20}

--- PianoDisplay.java
+++ PianoDisplay.java
@@ -15,7 +15,7 @@ public class PianoDisplay extends GCanvas implements ComponentListener, PianoCon
 	public void update() {
 		removeAll();
 		double distBtwn = getWidth() / NUM_WHITE;
-		double widthKey = getWidth() / NUM_WHITE * WB_RATIO;
+		double widthKey = 2 * getWidth() / (3 * NUM_WHITE);
 		setUpFirstBlackSet(distBtwn, widthKey);
 		setUpSecondBlackSet(distBtwn, widthKey);
 		drawWhiteKeyLines();
