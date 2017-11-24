55bae0ed6191a166305e6696465b0a6d71b049c1
S <s>
2017-11-24 10:34:37 -0800

    {"types":"C","runs":1}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -8,7 +8,7 @@ import acm.util.*;
 import acm.graphics.*;
 import java.applet.*;
 
-public class GraphicsContest extends GraphicsProgram {
+public class GraphicsContest extends Program {
 
 	public void run() {
 		AudioClip cFour = MediaTools.loadAudioClip("Marimba.yarn.ff.C4.stereo.aif");
