09497dc336b9f2bafd7fcd495f4a294cffb5ee41
S <s>
2017-11-24 12:54:13 -0800

    {"types":"C","runs":28}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -22,10 +22,11 @@ public class GraphicsContest extends Program implements PianoConstants {
 	}
 	
 	public void mouseClicked(MouseEvent e) {
-		createBottomHalfSounds(e.getX(), e.getY());
+		removeAll();
+		//createBottomHalfSounds(e.getX(), e.getY());
 	}
 	
-	private void createBottomHalfSounds(int x, int y) {
+	/*private void createBottomHalfSounds(int x, int y) {
 		if (x > 0 && x < getWidth() / NUM_WHITE && y > getHeight() / 2 && y < getHeight()) {
 			AudioClip cFour = MediaTools.loadAudioClip("Marimba.yarn.ff.C4.stereo.aif");
 			cFour.play();
