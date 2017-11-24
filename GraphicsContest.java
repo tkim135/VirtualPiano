769604af80573a9e13284876883276a2379a7a4e
S <s>
2017-11-24 13:02:15 -0800

    {"types":"C","runs":33}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -20,21 +20,19 @@ public class GraphicsContest extends GraphicsProgram implements ComponentListene
 		
 		addMouseListeners();
 		
-		//AudioClip cFour = MediaTools.loadAudioClip("Marimba.yarn.ff.C4.stereo.aif");
-		//cFour.play();
+		
 	}
 	
 	public void mouseClicked(MouseEvent e) {
-		AudioClip cFour = MediaTools.loadAudioClip("Marimba.yarn.ff.C4.stereo.aif");
-		cFour.play();
-		//createBottomHalfSounds(e.getX(), e.getY());
+		createBottomHalfSounds(e.getX(), e.getY());
 	}
 	
-	/*private void createBottomHalfSounds(int x, int y) {
+	private void createBottomHalfSounds(int x, int y) {
 		if (x > 0 && x < getWidth() / NUM_WHITE && y > getHeight() / 2 && y < getHeight()) {
-			
+			AudioClip cFour = MediaTools.loadAudioClip("Marimba.yarn.ff.C4.stereo.aif");
+			cFour.play();
 		}
-	}*/
+	}
 	
 	public void update() {
 		removeAll();
